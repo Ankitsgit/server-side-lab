26174ad45067bdf35f5b9e6d8bd9069b7aad512e
Ankitsgit <ankit77us@gmail.com>
2025-06-25 09:38:16 +0530

    basic authentication completed

---
+++ controllers/user.js
@@ -0,0 +1,34 @@
+const User =require("../models/user");
+
+const {} = require("uuid");
+
+async function handleUserSignup(req,res){
+    const{name,email,password}=req.body;
+    await User.create({
+        name,
+        email,
+        password,
+    });
+
+    return res.redirect("/");
+}
+
+
+async function handleUserLogin(req ,res){
+    const { email , password} = req.body;
+
+    const user = await User.findOne({email , password});
+    if(!user) return res.render("login" , {
+        error:"Invalid Username or Password",
+    });
+
+
+    return res.redirect("/");
+    
+}
+
+module.exports = {
+    handleUserSignup,
+    handleUserLogin,
+
+};
---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const path = require('path')
+
+
+const { connectToMongoDB } = require("./connect");
+const URL = require("./models/url");
+
+
+// export routes to use
+const urlRoute = require("./routes/url");
+const staticRoute = require('./routes/staticRoute')
+const userRoute =require("./routes/user");
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
+  console.log("MongoDB connected")
+);
+
+
+// telling express that using ejs
+app.set("view engine" , "ejs")
+app.set("views" , path.resolve("./views"))
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false})) //for form data
+
+// registering routes
+app.use("/url", urlRoute);
+app.use("/" , staticRoute);
+app.use("/user",userRoute);
+
+// Server Side Rendering -> 
+// Write html on server side -> complicated
+// For ease we use EJS
+app.get('/test' , async(req , res)=>{
+  const allUrls = await URL.find({});
+  // return res.end(`
+  //   <html>
+  //     <head></head>
+  //     <body>
+  //       <ol>
+  //         ${allUrls.map( url => `<li>
+  //           ${url.shortId} - 
+  //           ${url.redirectURL} - 
+  //           ${url.visitHistory.length}</li>`
+  //           ).join("")
+  //           }
+  //       </ol>
+  //     </body>
+  //   </html>`
+    
+  // )
+  
+  return res.render('home',{
+    urls: allUrls,
+  })
+})
+
+
+
+// Redirect logic
+app.get("/:shortId", async (req, res) => {
+  try {
+    const shortId = req.params.shortId;
+
+    const entry = await URL.findOneAndUpdate(
+      { shortId },
+      {
+        $push: {
+          visitHistory: {
+            timestamp: Date.now(),
+          },
+        },
+      },
+      { new: true }
+    );
+
+    if (!entry) {
+      return res.status(404).send("Short URL not found");
+    }
+
+    res.redirect(entry.redirectURL);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
